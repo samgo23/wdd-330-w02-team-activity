def2bc202f7811de1129780c1e4359cfc03240f6
samgo23 <gordsam18@gmail.com>
2023-01-13 18:08:11 -0700

    chnaged the addProductToCart function to an array

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = setLocalStorage("so-cart") || [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
