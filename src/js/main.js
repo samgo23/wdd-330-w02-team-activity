88174659c95006ea85cb20a278c41c140bd7758f
sventura102 <sventura102@gmail.com>
2023-01-27 21:19:37 -0800

    created main to add list of products to index

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list")
+const productList = new ProductList("Tents", dataSource, listElement);
+productList.init();
+
