332a833a3d728d968f3a0e7b72fa60292872f4c6
BelChaparro <a.belenchaparro@gmail.com>
2023-02-01 02:13:55 -0300

    run "npm run format" to correct formatting issues

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const listElement = document.querySelector(".product-list")
+const listElement = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, listElement);
 productList.init();
-
