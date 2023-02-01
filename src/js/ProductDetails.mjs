6c17ca6452f1affd21f3221735a73aecaf69248f
BelChaparro <a.belenchaparro@gmail.com>
2023-02-01 02:12:42 -0300

    added $ to price, changed ' ' for " ", removed product parameter from add to cart function

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -11,7 +11,7 @@ function productDetailsHTML(product){
       alt="${product.NameWithoutBrand}"
     />
 
-    <p class="product-card__price">${product.FinalPrice}</p>
+    <p class="product-card__price">$${product.FinalPrice}</p>
 
     <p class="product__color">${product.Colors.ColorName}</p>
 
@@ -38,17 +38,15 @@ export default class ProductDetails {
     this.renderProductDetails(".product-detail");
   // once the HTML is rendered we can add a listener to Add to Cart button
   // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document.getElementById('addToCart')
-    .addEventListener('click', this.addProductToCart.bind(this));
+    document.getElementById("addToCart")
+    .addEventListener("click", this.addProductToCart.bind(this));
     
     }
     
-    addProductToCart(product) {
-    
-        let cart = getLocalStorage("so-cart") || [];
-      
-        cart.push(product);
-        setLocalStorage("so-cart", cart);
+    addProductToCart() {    
+      let cart = getLocalStorage("so-cart") || [];
+      cart.push(this.product);
+      setLocalStorage("so-cart", cart);
       }
 
     renderProductDetails(selector){
