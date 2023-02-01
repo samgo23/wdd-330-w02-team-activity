c88f6dd8ec33ce1b89447968e8dcd8074fd79f97
BelChaparro <a.belenchaparro@gmail.com>
2023-02-01 02:11:29 -0300

    removed functions that were added to the generic productDetails rendering js

--- src/js/product.js
+++ src/js/product.js
@@ -6,20 +6,3 @@ const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
-
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart") || [];
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
