75fdd4a893ad2cf6ba1c92fdadab4e29665a0bf8
BelChaparro <a.belenchaparro@gmail.com>
2023-02-23 22:36:08 -0300

    removed blank line

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,7 +5,6 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 loadHeaderFooter();
 
 const category = getParam("category");
-
 const dataSource = new ExternalServices();
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList(category, dataSource, listElement);
