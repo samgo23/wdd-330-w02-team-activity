dfefe9b5c75b6521df6f2e468c25fa8ebd64d72b
BelChaparro <a.belenchaparro@gmail.com>
2023-03-03 00:39:44 -0300

    corrected images src to match API object to request medium images for prods.

--- src/js/ShoppingCart.js
+++ src/js/ShoppingCart.js
@@ -4,7 +4,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
     <a href="/product_pages/index.html?product=${item.Id}" class="cart-card__image">
       <img
-        src="${item.Image}"
+        src="${item.Images.PrimaryMedium}"
         alt="${item.Name}"
       />
     </a>
