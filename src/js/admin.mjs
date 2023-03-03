d7b120abd1faa36128d809d1e0f5f2d7dbc44caa
sventura102 <sventura102@gmail.com>
2023-03-03 00:15:46 -0800

    modified class, imported added external services, grabbed user info, and modified button to check user input

--- src/js/admin.mjs
+++ src/js/admin.mjs
@@ -1,10 +1,30 @@
 import { getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 
-function login (){
-    
-};
+export default class Admin {
+    constructor(outputSelector) {
+      this.mainElement = document.querySelector(outputSelector);
+      this.token = null;
+      this.services = new ExternalServices();
+    }
 
-function showLogin() {
-    const email = document.querySelector("#email");
-    const password = document.querySelector("#password");
+      async login(user, next) {
+      // I built the login method with a callback: next. 
+      // This makes it much more flexible...
+      // there could be many different things the user wants to do after logging in...
+      // this allows us that flexibility without having to write a bunch of login methods
+      try {
+        this.token = await this.services.loginRequest(user);
+        next()
+      } 
+      catch(err) {
+        // remember this from before?
+        alertMessage(err.message.message);
+      }
+    };
 }
+function showLogin() {
+document.querySelector("#loginButton").addEventListener("click", (e) => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    this.login({ email, password })}); }
