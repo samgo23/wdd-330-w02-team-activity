5fc03c5d4c646bc1872f65ad098ecd88b1df03b9
sventura102 <sventura102@gmail.com>
2023-03-02 21:24:03 -0800

    created admin html file, added form, and submit button

---
+++ src/js/admin.mjs
@@ -0,0 +1,10 @@
+import { getLocalStorage } from "./utils.mjs";
+
+function login (){
+    
+};
+
+function showLogin() {
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password");
+}
