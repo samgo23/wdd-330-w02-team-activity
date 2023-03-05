2b4fa31e57fcaeca8574529be5f14f0351e28b5e
BelChaparro <a.belenchaparro@gmail.com>
2023-03-05 05:11:45 -0300

    created funtion to create html template for login form + login method

--- src/js/admin.mjs
+++ src/js/admin.mjs
@@ -1,18 +1,36 @@
 import { getLocalStorage } from "./utils.mjs";
 
 export default class ExternalServices {
-    constructor() {
-      
+    constructor(outputselector) {
+        this.mainElement = document.querySelector(outputSelector);
+        this.token = null;
+        this.services = new ExternalServices();
     }
-    async init () {}
 
-     login (){
-        
+    async login (creds, next){
+        try {
+            this.token = await this.services.loginRequest(creds);
+            next()
+        } 
+        catch(err) {
+          alertMessage(err.message.message);
+        }
     }
 
     showLogin() {
-        document.querySelector(".Login").innerHTML =
-            `<form id="login-form name="login">
+        // Add HTML for the login form:
+        document.querySelector(".Login").innerHTML = loginFormHTML();            
+
+        // Listen for a click on the button to call the login method:
+        document.querySelector("#login-submit").addEventListener("click", (e) => { 
+            const email = document.querySelector("#email").value;
+            const password = document.querySelector("#password").value;
+            this.login({email, password}) });
+    }
+}
+
+function loginFormHTML() {
+    return `<form id="login-form name="login">
                 <label for="email">E-mail:</label>
                 <input type="email" name="email" id="email" required>
 
@@ -21,8 +39,4 @@ export default class ExternalServices {
 
                 <button type="submit" id="login-submit">Log In</button>
             </form>`;
-
-        // listening for click on the button to call the login method:
-        document.querySelector("#login-submit").addEventListener("click", (e) => { e.login() });
-    }
 }
