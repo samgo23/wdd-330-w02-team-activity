9a3c9d178479dcad4b391c198e34ec6f4ddb4a9a
BelChaparro <a.belenchaparro@gmail.com>
2023-03-05 03:01:40 -0300

    renamed form attributes values

--- src/js/admin.mjs
+++ src/js/admin.mjs
@@ -13,25 +13,16 @@ export default class ExternalServices {
     showLogin() {
         document.querySelector(".Login").innerHTML =
             `<form id="login-form name="login">
-                <label for="email-input">E-mail:</label>
-                <input type="text" name="email-input" id="email" required />
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" id="email" required>
 
-                <label for="password-input">Password:</label>
-                <input type="text" name="password-input" id="password" required />
+                <label for="password">Password:</label>
+                <input type="text" name="password" id="password" required>
 
                 <button type="submit" id="login-submit">Log In</button>
             </form>`;
 
-        // listening for click on the button:
-        document.querySelector("#login-submit").addEventListener("click", (e) => {
-            e.preventDefault();
-            // Check form validity (no empty input fields):
-            var myForm = document.forms[0];
-            var chk_status = myForm.checkValidity();
-            myForm.reportValidity();
-            if (chk_status) {
-            myCheckout.checkout();
-            }
-        });
+        // listening for click on the button to call the login method:
+        document.querySelector("#login-submit").addEventListener("click", (e) => { e.login() });
     }
 }
