7d1a9137535d57075a8bbec81f3c71f03ab2aafd
BelChaparro <a.belenchaparro@gmail.com>
2023-03-04 21:47:50 -0300

    added showLogin method

--- src/js/admin.mjs
+++ src/js/admin.mjs
@@ -1,10 +1,37 @@
 import { getLocalStorage } from "./utils.mjs";
 
-function login (){
-    
-};
+export default class ExternalServices {
+    constructor() {
+      
+    }
+    async init () {}
 
-function showLogin() {
-    const email = document.querySelector("#email");
-    const password = document.querySelector("#password");
+     login (){
+        
+    }
+
+    showLogin() {
+        document.querySelector(".Login").innerHTML =
+            `<form id="login-form name="login">
+                <label for="email-input">E-mail:</label>
+                <input type="text" name="email-input" id="email" required />
+
+                <label for="password-input">Password:</label>
+                <input type="text" name="password-input" id="password" required />
+
+                <button type="submit" id="login-submit">Log In</button>
+            </form>`;
+
+        // listening for click on the button:
+        document.querySelector("#login-submit").addEventListener("click", (e) => {
+            e.preventDefault();
+            // Check form validity (no empty input fields):
+            var myForm = document.forms[0];
+            var chk_status = myForm.checkValidity();
+            myForm.reportValidity();
+            if (chk_status) {
+            myCheckout.checkout();
+            }
+        });
+    }
 }
